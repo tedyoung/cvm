744144d7e0ee428bb91b114d26d46448b96bdf4b
Ted M. Young <tedyoung@gmail.com>
2018-11-12 08:57:45 -0800

    Spring Intensive Lab 8 solution with size bonusˆ

--- src/main/java/com/welltestedlearning/cvm/CoffeeOrderWebController.java
+++ src/main/java/com/welltestedlearning/cvm/CoffeeOrderWebController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CoffeeOrderWebController {
@@ -33,4 +35,18 @@ public class CoffeeOrderWebController {
     return "coffee-order-view";
   }
 
+  @GetMapping("/order-coffee")
+  public String orderCoffeeForm() {
+    return "order-coffee-form";
+  }
+
+  @PostMapping("/order-coffee")
+  public String processOrderCoffee(@ModelAttribute("name") String name, @ModelAttribute("size") String size) {
+    CoffeeOrder coffeeOrder = new CoffeeOrder();
+    coffeeOrder.changeNameTo(name);
+    coffeeOrder.size(SizeOption.valueOf(size.toUpperCase()));
+    CoffeeOrder savedOrder = coffeeOrderRepository.save(coffeeOrder);
+
+    return "redirect:/coffee-order/" + savedOrder.getId();
+  }
 }
